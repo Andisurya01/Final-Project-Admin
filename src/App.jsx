e1d33fd029da1a84089c22e8860f5350e8e77c21
Andisurya01 <andi.suryapriyadi@gmail.com>
2023-11-23 17:16:26 +0700

    [try] ubah path login

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,32 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login />}></Route>
+          <Route path='/' element={<Login />}></Route>
         </Routes>
         <div >
-                <div className="hidden">
-                    <div className="absolute w-screen py-10 bg-black bg-opacity-90">
-                        <TambahKelas></TambahKelas>
-                    </div>
-                </div>
-                <div className="flex">
-                    <div className="">
-                        <SideBar></SideBar>
-                    </div>
-                    <div className="w-screen">
-                        <Navbar></Navbar>
-                        <div className="flex flex-row gap-6 justify-between px-16 py-20">
-                            <Card number={"450"} title={"Active Users"} background={"#489CFF"}></Card>
-                            <Card number={"25"} title={"Active Class"} background={"#73CA5C"}></Card>
-                            <Card number={"20"} title={"Premium Class"} background={"#6148FF"}></Card>
-                        </div>
-                        <Routes>
-                            <Route path='/dashboard' element={<DashBoard />}></Route>
-                            <Route path='/kelolakelas' element={<KelolaKelas />}></Route>
-                        </Routes>
-                    </div>
-                </div>
+          <div className="hidden">
+            <div className="absolute w-screen py-10 bg-black bg-opacity-90">
+              <TambahKelas></TambahKelas>
             </div>
+          </div>
+          <div className="flex">
+            <div className="">
+              <SideBar></SideBar>
+            </div>
+            <div className="w-screen">
+              <Navbar></Navbar>
+              <div className="flex flex-row gap-6 justify-between px-16 py-20">
+                <Card number={"450"} title={"Active Users"} background={"#489CFF"}></Card>
+                <Card number={"25"} title={"Active Class"} background={"#73CA5C"}></Card>
+                <Card number={"20"} title={"Premium Class"} background={"#6148FF"}></Card>
+              </div>
+              <Routes>
+                <Route path='/dashboard' element={<DashBoard />}></Route>
+                <Route path='/kelolakelas' element={<KelolaKelas />}></Route>
+              </Routes>
+            </div>
+          </div>
+        </div>
       </BrowserRouter>
     </>
   )
